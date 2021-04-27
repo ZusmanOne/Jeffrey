50b07d9d4105b59e4fb622efb1c94ed42c8d45dd
Nikita <bigmama93@mail.ru>
2021-04-27 17:02:46 +0300

    создан_шаблон_бустрапа_с_навбаром(боковым)_хдэром_и_футером

--- Flat/Flat/settings.py
+++ Flat/Flat/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-ru'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

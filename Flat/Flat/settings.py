7d6d6cee1dffcabd525898d3e5c5e9d09bed4e14
Nikita <bigmama93@mail.ru>
2021-07-06 11:17:06 +0300

    with_postgresql

--- Flat/Flat/settings.py
+++ Flat/Flat/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'Flat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alisadb',
+        'USER': 'postgres',
+        'PASSWORD' : 'Alisa1905',
+        'HOST': 'localhost',
+        'PORT' : '5432'
     }
 }
 
